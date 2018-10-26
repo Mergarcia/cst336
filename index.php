51c754df50a280594a194175885ed01ee1b997c2
Mercedes Garcia <mergarcia@csumb.edu>
2018-10-26 01:37:05 +0000

    adding file

--- index.php
+++ index.php
@@ -4,8 +4,12 @@
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo "CST 336 Homepage";
+echo "Mercedes Garcia's CST 336 Homepage";
+
 
 ?>
+    <br /><br />
+    
+    <a href="Lab1/index.html"> Lab 1</a>
 </body>
 </html>
