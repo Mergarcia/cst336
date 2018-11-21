962a0ea2ab204d2ebe54057db981f50e480843f9
Mercedes Garcia <mergarcia@csumb.edu>
2018-11-21 07:38:18 +0000

    adding message

--- Labs/Lab5/dbConnection.php
+++ Labs/Lab5/dbConnection.php
@@ -1,11 +1,21 @@
 <?php
-function getDatabaseConnection($dbname = 'ottermart') {
+function getDatabaseConnection($dbname = 'heroku_8ed61068ca05413') {
     
-    $host = 'localhost'; 
-    $username = 'root';
-    $password = '';
+    $host = 'us-cdbr-iron-east-01.cleardb.net'; //cloud9
+    //$dbname = 'tcp';
+    $username = 'ba015ab516ccf5';
+    $password = 'b2c23509';
+
+
+    $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    // display error when accessing tables
+    $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
-    // creates db connection
+    return $dbConn;
+    
+}
+
+// creates db connection
     if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
         $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
         $host = $url["host"];
@@ -13,13 +23,3 @@ function getDatabaseConnection($dbname = 'ottermart') {
         $username = $url["user"];
         $password = $url["pass"];
     } 
-    
-    $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    // display error when accessing tables
-    
-    $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    return $dbConn;
-    
-}
-
