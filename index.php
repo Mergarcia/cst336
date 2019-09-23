4001fb49d0cc1f99b35f0998fa757bc13c18504e
EC2 Default User <ec2-user@ip-172-31-19-110.us-west-2.compute.internal>
2019-09-23 08:08:02 +0000

    added new info to index

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ echo "Mercedes Garcia's CST 336 Homepage";
 ?>
     <br /><br />
     
-    <a href="HW/HW1/index.php"> Homework 1 </a> Not Working
+    <a href="HW/HW1/index.php"> Homework 1 </a> 
     <br /><br />
    
     <br /><br />
