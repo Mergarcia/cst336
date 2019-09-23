dafe38cfcffdf2cc5f73804e462497da20f16f8b
EC2 Default User <ec2-user@ip-172-31-19-110.us-west-2.compute.internal>
2019-09-23 07:41:10 +0000

    added new info to index

--- index.php
+++ index.php
@@ -10,29 +10,29 @@ echo "Mercedes Garcia's CST 336 Homepage";
 ?>
     <br /><br />
     
-    <a href="HW1/index.php"> Homework 1 </a>
+    <a href="HW/HW1/index.php"> Homework 1 </a>
+    echo "<br>";
+    <a href="HW/HW2/index.php"> Homework 2 </a>
+    echo "<br>";
+    <a href="HW/HW3/index.php"> Homework 3 </a>
+    echo "<br>";
+    <a href="HW/HW4/index.php"> Homework 4 </a>
+    echo "<br>";
+    <a href="Labs/Lab1/index.php"> Lab 1 </a>
     
-    <a href="HW2/index.php"> Homework 2 </a>
+    <a href="Labs/Lab2/index.php"> Lab 2 </a>
     
-    <a href="HW3/index.php"> Homework 3 </a>
+    <a href="Labs/Lab3/index.php"> Lab 3 </a>
     
-    <a href="HW4/index.php"> Homework 4 </a>
+    <a href="Labs/Lab4/index.php"> Lab 4 </a>
     
-    <a href="Lab1/index.php"> Lab 1 </a>
+    <a href="Labs/Lab5/index.php"> Lab 5 </a>
     
-    <a href="Lab2/index.php"> Lab 2 </a>
+    <a href="Labs/Lab6/index.php"> Lab 6 </a>
     
-    <a href="Lab3/index.php"> Lab 3 </a>
+    <a href="Labs/Lab7/index.php"> Lab 7 </a>
     
-    <a href="Lab4/index.php"> Lab 4 </a>
-    
-    <a href="Lab5/index.php"> Lab 5 </a>
-    
-    <a href="Lab6/index.php"> Lab 6 </a>
-    
-    <a href="Lab7/index.php"> Lab 7 </a>
-    
-    <a href="Lab8/index.php"> Lab 8 </a>
+    <a href="Labs/Lab8/index.php"> Lab 8 </a>
     
     <a href="Final_Project/index.php"> Final Project </a>
 
