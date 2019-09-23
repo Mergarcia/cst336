cf929a828f833b7a6721d79c6bf6eaa43f9c1caa
EC2 Default User <ec2-user@ip-172-31-19-110.us-west-2.compute.internal>
2019-09-23 07:45:04 +0000

    added new info to index

--- index.php
+++ index.php
@@ -11,31 +11,31 @@ echo "Mercedes Garcia's CST 336 Homepage";
     <br /><br />
     
     <a href="HW/HW1/index.php"> Homework 1 </a>
-    echo "<br>";
+    <br>
     <a href="HW/HW2/index.php"> Homework 2 </a>
-    echo "<br>";
+    <br>
     <a href="HW/HW3/index.php"> Homework 3 </a>
-    echo "<br>";
+    <br>
     <a href="HW/HW4/index.php"> Homework 4 </a>
-    echo "<br>";
+    <br>
     <a href="Labs/Lab1/index.php"> Lab 1 </a>
-    
+    <br>
     <a href="Labs/Lab2/index.php"> Lab 2 </a>
-    
+    <br>
     <a href="Labs/Lab3/index.php"> Lab 3 </a>
-    
+    <br>
     <a href="Labs/Lab4/index.php"> Lab 4 </a>
-    
+    <br>
     <a href="Labs/Lab5/index.php"> Lab 5 </a>
-    
+    <br>
     <a href="Labs/Lab6/index.php"> Lab 6 </a>
-    
+    <br>
     <a href="Labs/Lab7/index.php"> Lab 7 </a>
-    
+    <br>
     <a href="Labs/Lab8/index.php"> Lab 8 </a>
-    
+    <br>
     <a href="Final_Project/index.php"> Final Project </a>
-
+    <br>
     
 </body>
 </html>
